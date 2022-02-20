983aea9d44496613e50c6f958c7c733c39899c64
game54 <ap_nbs@hotmail.com>
2022-02-21 01:42:15 +0200

    up

--- app.js
+++ app.js
@@ -370,7 +370,7 @@ ${starttimermin.value || "empty"}
 
   // Convert the text to BLOB.
   const textToBLOB = new Blob([data], { type: "text/plain" });
-  const sFileName = "formData.txt"; // The file to save the data.
+  const sFileName = "settings.txt"; // The file to save the data.
 
   let newLink = document.createElement("a");
   newLink.download = sFileName;
